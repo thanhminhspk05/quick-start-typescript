175460276dd4f6ee3b5e4464e2c9aaa85f61c43a
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-08-11 10:16:19 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Button, ButtonGroup, Stack, Typography } from '@mui/material';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Typography />
+
+      <ButtonGroup />
+
+      <Button />
+
+      <Stack />
+    </>
+  );
+}
+
+export default App;
