ecec6d86f94658ec053c664b8645ca31bea04188
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-09-11 14:50:18 +0700

    remove

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,8 @@
 import axios from 'axios';
-import { useCookies } from 'react-cookie';
 
 const DashboardPage = () => {
-  const [cookies, setCookie] = useCookies(['access_token', 'refresh_token']);
-  console.log(cookies);
   const handleLogout = async () => {
-    console.log('logout');
     const response = await axios.get('http://localhost:8000/api/v1/logout', { withCredentials: true });
-    console.log({ response });
   };
 
   return (
